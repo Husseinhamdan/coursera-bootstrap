390d736fb70af676170af43d9dd082a552b881ba
Hussein Hamdan <hamdanhussein51@gmail.com>
2022-09-06 19:17:09 +0300

    Assignment four

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function loginModal() {
+    $("#loginModal").modal('show')
+}
+function ReverseModal() {
+    $("#reserveModal").modal('show')
+
+}
+function closeLoginModal() {
+    $("#loginModal").modal('hide')
+}
+function closeReverseModal() {
+    $("#reserveModal").modal('hide')
+
+}
